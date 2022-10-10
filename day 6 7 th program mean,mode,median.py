b8adc412d54e613bc0b10692c73c5aa8498b24e0
Vivek18v <112846370+Vivek18v@users.noreply.github.com>
2022-10-10 22:09:35 +0530

    Add files via upload

---
+++ day 6 7 th program mean,mode,median.py
@@ -0,0 +1,8 @@
+size = float(input('No.of elements :'))
+data = [float(input()) for i in range(1,size+1)]
+mean = sum(data)/size
+data.sort()
+median = data[int(size/2)]
+print('Mean :', '{:.2f}'.format(mean))
+print('Median :',median)
+print('Mode :',max(set(data),key = data.count))
