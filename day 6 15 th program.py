3741c14d1dd9fbc689c2795fb722427180cba328
Vivek18v <112846370+Vivek18v@users.noreply.github.com>
2022-10-10 10:38:06 +0530

    Add files via upload

---
+++ day 6 15 th program.py
@@ -0,0 +1,10 @@
+num = float(input("enter the number:"))
+result = 0
+for i in (1,num):
+    if(num%i)==	0:
+        result = result+i
+if result==num:
+    print(num,"it is a perfect number")
+
+else:
+     print(num,"it is not a perfect number")
